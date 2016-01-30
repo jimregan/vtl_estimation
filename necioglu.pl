952409bfbed2b58660965a89b70eb66111c6319a
Jim O'Regan <joregan@gmail.com>
2016-01-30 17:22:22 +0000

    spaces to tabs

--- necioglu.pl
+++ necioglu.pl
@@ -2,19 +2,19 @@
 use warnings;
 use strict;
 sub vtl {
-	my $f_one = fone($_[0]);
-	my $v = 35000;
-	return ($v / (4 * $f_one));
+    my $f_one = fone($_[0]);
+    my $v = 35000;
+    return ($v / (4 * $f_one));
 }
 sub fone {
-	my $fk = $_[0];
-	my $M = $#$fk + 1;
-	my $acc = 0;
-	for (my $i = 1; $i < $M+1; $i++) {
-		my $part = ($$fk[$i-1] / ((2 * $i) -1));
-		$acc += $part * $part;
-	}
-	return sqrt ($acc / $M);
+    my $fk = $_[0];
+    my $M = $#$fk + 1;
+    my $acc = 0;
+    for (my $i = 1; $i < $M+1; $i++) {
+        my $part = ($$fk[$i-1] / ((2 * $i) -1));
+        $acc += $part * $part;
+    }
+    return sqrt ($acc / $M);
 }
 my $f;
 if (!$ARGV[0] || $ARGV[0] eq '') {
